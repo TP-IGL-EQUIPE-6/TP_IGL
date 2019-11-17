5fac025d0a4ec24e57cebdda5d16d544fca481f1
BENNAMANE Khadidja <hk_bennamane@esi.dz>
2019-11-17 21:47:39 +0100

    first commit

---
+++ resources/views/inc/navbar.blade.php
@@ -0,0 +1,40 @@
+<nav >
+<a href="#">{{config('app.name', 'Scolarité')}}</a>
+        <button type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+          <span></span>
+        </button>
+      
+        <div id="navbarsExampleDefault">
+          <ul>
+            <li>
+              <a href="/TP_IGL/public/">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li >
+              <a href="/TP_IGL/public/about">About</a>
+            </li>
+            <li >
+                <a href="/TP_IGL/public/AfficherPlanning">AfficherPlanning</a>
+            </li>
+            
+            <!--
+            <li >
+              <a href="" tabindex="-1" aria-disabled="true">Disabled</a>
+            </li>
+            <li >
+              <a href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+              <div aria-labelledby="dropdown01">
+                <a href="#">Action</a>
+                <a href="#">Another action</a>
+                <a href="#">Something else here</a>
+              </div>
+            </li>
+          </ul>
+        -->
+
+          <form>
+            <input type="text" placeholder="Search" aria-label="Search">
+            <button type="submit">Search</button>
+          </form>
+        </div>
+      </nav>
+      
---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1> ABOUT OUR APPLICATION </h1> 
+    <p> hjshd skdjskd skjdoqidso oazieoa ds oqidpqod pqospq </p>
+
+@endsection
+
