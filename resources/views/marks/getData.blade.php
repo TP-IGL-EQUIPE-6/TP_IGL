4aeea2377b923b13a3ca95023e4a069159fc349f
khebizi <ha_khebizi@esi.dz>
2019-11-23 01:23:59 +0100

    tableau d'affichage

--- resources/views/marks/getData.blade.php
+++ resources/views/marks/getData.blade.php
@@ -14,7 +14,7 @@ th, td {
 tr:hover {background-color: #f5f5f5;}
 
 th {
-  background-color: #4CAF50;
+  background-color: rgb(199, 53, 27);
   color: white;
 }
 </style>
@@ -23,7 +23,6 @@ th {
 
 @section('content')
     <h1>Affichage liste des etudiants avec moyenne</h1>
-<<<<<<< HEAD
 
     @foreach($student as $etudiant)      
         <h3>Matricule : {{$etudiant -> id}} </br>
@@ -38,33 +37,3 @@ th {
 @endsection
 
 
-=======
-    <table id="example" class="display" style="width:100%">
-    <thead>
-            <tr>
-                <th>Matricule</th>
-                <th>nom</th>
-                <th>prenom</th>
-                <th>niveau</th>
-                <th>groupe</th>
-                <th>moyenne générale</th>
-            </tr>
-        </thead>
-        <tbody>
-                @foreach($student as $etudiant)
-                <tr>
-                <td>{{$etudiant -> id}}</td>
-                <td>{{$etudiant -> nom}}</td>
-                <td>{{$etudiant -> prenom}}</td>
-                <td>{{$etudiant -> niveau}}</td>
-                <td>{{$etudiant -> grp}}</td>
-                
-                 </tr>
-                @endforeach
-         </tbody>
-</table>
-@endsection
-
-
-
->>>>>>> sauvegarde de modif
