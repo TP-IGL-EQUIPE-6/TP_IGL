a54e986dc5873bf532a0f75c7a7119b7c02af3e6
khebizi <ha_khebizi@esi.dz>
2020-01-03 15:58:54 +0100

    test untaire pour l'ajout d'un etudiant

--- tests/Feature/StudentTest.php
+++ tests/Feature/StudentTest.php
@@ -1,6 +1,6 @@
 <?php
 
-/*
+
 namespace Tests\Feature;
 
 use Tests\TestCase;
@@ -10,34 +10,25 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class StudentTest extends TestCase
 {
-    //use DatabaseMigrations;
-    /** @test 
-    public function student_can_be_created(){
-    $user = factory(App\User::class)->create();
-        $e = $user->Etudiant()->create([
+    
+    public function TestCreateStudentTest(){
+        $data = [
             'nom'=>'Khebizi',
             'prenom'=>'ahlem',
             'niveau'=>'1cs',
             'section'=>'B',
             'Groupe'=> 9,
-        ]);
-    
-    $found_etudiant=Etudiant::found(1);
-    $this.assertEquals($found_etudiant->nom,'khebizi');
-    $this.assertEquals($found_etudiant->prenom,'ahlem');
-    $this.assertEquals($found_etudiant->niveau,'1cs');
-    $this.assertEquals($found_etudiant->section,'B');
-    $this.assertEquals($found_etudiant->groupe,9);
-}
-    /**
-     * A basic test example.
-     *
-     * @return void
-    
-    public function testExample()
-    {
-        $this->assertTrue(true);
-    }
+        ];
+$user = factory(\App\User::class)->create();
+$response = $this->actingAs($user, 'api')->json('POST', '/students/CreerEtudiantForm',$data);
+$response->assertStatus(200);
+$response->assertJson(['status' => true]);
+$response->assertJson(['message' => "Product Created!"]);
+$response->assertJson(['data' => $data]);
+   
 }
 
-*/
+     
+}
+
+
